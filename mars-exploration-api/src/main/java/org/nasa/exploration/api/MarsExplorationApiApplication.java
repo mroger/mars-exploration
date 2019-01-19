610f2e4e895d92431e54ec47d7528b52bf52943a
Marcos R Oliveira <mroger.oliveira@gmail.com>
2019-01-19 15:45:39 -0200

    Adding API project skeleton

---
+++ mars-exploration-api/src/main/java/org/nasa/exploration/api/MarsExplorationApiApplication.java
@@ -0,0 +1,14 @@
+package org.nasa.exploration.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarsExplorationApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarsExplorationApiApplication.class, args);
+	}
+
+}
+
---
+++ mars-exploration-api/src/test/java/org/nasa/exploration/api/MarsExplorationApiApplicationTests.java
@@ -0,0 +1,17 @@
+package org.nasa.exploration.api;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MarsExplorationApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
